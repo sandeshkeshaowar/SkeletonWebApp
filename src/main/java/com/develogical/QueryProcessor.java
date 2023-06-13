bcafe9f07bbe0c56e5110792218211c31ae96411
sandeshkeshaowar <48853038+sandeshkeshaowar@users.noreply.github.com>
2023-06-13 14:50:25 +0000

    com-2

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,10 +6,11 @@ public class QueryProcessor {
 
     System.out.println("Received query:" + query);
 
-    if (query.toLowerCase().contains("sandesh")) {
-      return "Sandesh Keshaowar (26 April 1998 - 23 April 2048) is an "
-          + "English poet, playwright, and actor, widely regarded as the greatest "
-          + "writer in the English language and the world's pre-eminent dramatist.";
+    if (query.toLowerCase().contains("What is 39 plus 49 plus 1")) {
+      return 39 + 49 + 1;
+      //return "Sandesh Keshaowar (26 April 1998 - 23 April 2048) is an "
+       //   + "English poet, playwright, and actor, widely regarded as the greatest "
+       //   + "writer in the English language and the world's pre-eminent dramatist.";
     }
 
     return "";
