f1105a3d0fdd67bc105bb554e6eabf51c1fb3c75
sandeshkeshaowar <48853038+sandeshkeshaowar@users.noreply.github.com>
2023-06-13 14:46:18 +0000

    some changes

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,10 +7,10 @@ public class IndexPage extends HtmlPage {
   @Override
   protected void writeContentTo(PrintWriter writer) {
     writer.println(
-        "<h2>Welcome!</h2>"
+        "<h2 span style='color:blue'>Welcome All!</h2>"
             + "<p>Please enter your query in the box below: "
             + "<form><input type=\"text\" name=\"q\" />"
-            + "<input type=\"submit\">"
+            + "<input type=\"submit\" style=\" background-color: #04AA6D;border: none;color: white;padding: 16px 32px;text-decoration: none;margin: 4px 2px;cursor: pointer;\">"
             + "</form>"
             + "</p>");
   }
